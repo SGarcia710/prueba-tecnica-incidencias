ffcc997771cbed0cc3b228bb9d08a965150c9ce0
Sebastian Garcia <sgarcia710@outlook.com>
2021-05-06 15:34:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = React.useState();
+  const [error, setError] = React.useState(false);
+
+  const [biggerNumber, setBiggerNumber] = React.useState();
+  const onSubmit = (e) => {
+    if (!!error) {
+      setError(false);
+    }
+    e.preventDefault();
+    const length = number.toString().length;
+    if (length > 16 || length < 14) {
+      setError(true);
+      return;
+    }
+
+    const array = [...number.toString()];
+    console.log(array);
+
+    const repeats = {};
+
+    array.forEach((value) => {
+      const actualValue = repeats[value];
+      repeats[value] = !!actualValue ? actualValue + 1 : 1;
+    });
+
+    console.log(repeats);
+    const bigger = Object.keys(repeats).reduce((previous, current) => {
+      if (repeats[previous] < repeats[current]) {
+        return current;
+      } else {
+        return previous;
+      }
+    });
+
+    setBiggerNumber(bigger);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={(e) => {
+            setNumber(e.target.value);
+          }}
+          type="number"
+          placeholder={'Ingresa el número'}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      {!!error && (
+        <>
+          <span
+            style={{
+              color: 'red',
+            }}
+          >
+            El número debe tener de 14 a 16 caracteres
+          </span>
+          <br />
+        </>
+      )}
+
+      <span>El número que se repite más veces es: {biggerNumber}</span>
     </div>
   );
 }
